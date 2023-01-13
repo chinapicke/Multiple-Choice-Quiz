231193210c0a313fb652e495fb5c26da48c9158a
chinapicke <chinapickering@hotmail.co.uk>
2023-01-13 14:21:01 +0700

    working start button to show first question

---
+++ assets/js/logic.js
@@ -0,0 +1,53 @@
+// Link button in HTML to JSS so that the quiz starts once the button has been pressed, the first question will appear
+
+// This accesses the start id within HTML 
+let startBtn = document.getElementById("start")
+// This accss the h1 and p tag within the startscreen div within HTML 
+let startScreen =document.getElementById("start-screen")
+// This accesses the question containers
+let questionContainer = document.getElementById('questions')
+// This accesses the start id within HTML 
+let timerEl = document.getElementById("time")
+
+// This accesses the question title within the HTML 
+let questionTitle = document.getElementById("question-title")
+// This accesses the choices within the HTML 
+let choiceOne = document.getElementById("choice-1")
+let choiceTwo = document.getElementById("choice-2")
+let choiceThree= document.getElementById("choice-3")
+let choiceFour = document.getElementById("choice-4")
+// This accesses the final score within the HTML 
+let finalScore = document.getElementById("final-score")
+// This accesses the initials within the HTML
+let initialsEl = document.getElementById("initials")
+// This accesses the ol of highscores within the highscore HTML 
+let highScoreList = document.getElementById("highscores")
+
+
+//enable score to be added dependent if the answer is correct or not 
+let score = 0
+// create variable to set starting time to 60 seconds 
+let timeLeft = 60
+
+
+
+
+// Buttons 
+startBtn.addEventListener("click", startGame)
+
+function startGame() {
+    // To check that the button is functioning properly
+//     console.log("Game has been started")
+// Hide start button when it is clicked on so that the questions can be shown on the page 
+startBtn.classList.add('hide')
+// This also hides the start screen writing
+startScreen.classList.add('hide')
+// This shows the hidden questions
+questionContainer.classList.remove('hide')
+multipleQuestions()
+}
+
+
+function multipleQuestions(){
+    // Need to input object of questions
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,66 +1,59 @@
-// Link button in HTML to JSS so that the quiz starts once the button has been pressed, the first question will appear
+
+    // Create questions using get document by Element and then priniting it onto the page using textContent 
+    // We may also need to create new elements to go in the HTML using javascript and append these 
+    // With the questions we will need if statements to work out whether the user has gotten the correct answer or not 
+    // We need to create a further if statement to the above that will deduct time from the clock if it is incorrect
+
+    // Create object to store the datat of all the questions and the answers 
+    let quizData = [
+        {
+            questions: "What does HTML stand for?",
+            a: "HyperText Markup Language",
+            b: "HyperText Machine Language",
+            c: "HyperText Marking Language",
+            d: "HighText Marking Language",
+            correct: "a",
+        },
+        {
+            questions: "Which of the following keywords is used to define a variable in Javascript?",
+            a: "var",
+            b: "let",
+            c: "Both A and B",
+            d: "None of the above",
+            correct: "c",
+        },
+        {
+            questions: "Which element is used for or styling HTML layout?",
+            a: "CSS",
+            b: "jQuery",
+            c: "Javascript",
+            d: "PHP",
+            correct: "a",
+        },
+        {
+            questions: "What does <ul> stand for?",
+            a: "Ordered list",
+            b: "Unordered list",
+            c: "Undefined",
+            d: "None of the above",
+            correct: "b",
+        },
+        {
+            questions: "What does CSS stand for?",
+            a: "Cascading Style Sheets.",
+            b: "Coloured Special Sheets",
+            c: "Color and style sheets",
+            d: "None of the above",
+            correct: "a",
+        },
+        {
+            questions: "What does && expression mean?",
+            a: "Question",
+            b: "Or",
+            c: "Add",
+            d: "None of the above",
+            correct: "d",
+        },
+    ]
 
 
-
-
-
-
-
-// Create questions using get document by Element and then priniting it onto the page using textContent 
-// We may also need to create new elements to go in the HTML using javascript and append these 
-// With the questions we will need if statements to work out whether the user has gotten the correct answer or not 
-// We need to create a further if statement to the above that will deduct time from the clock if it is incorrect
-
-// Create object to store the datat of all the questions and the answers 
-let quizData=[
-    {
-        questions: "What does HTML stand for?",
-        a: "HyperText Markup Language" ,
-        b: "HyperText Machine Language",
-        c: "HyperText Marking Language",
-        d: "HighText Marking Language",
-        correct:"a",
-    },
-    {
-        questions: "Which of the following keywords is used to define a variable in Javascript?",
-        a: "var" ,
-        b: "let",
-        c: "Both A and B",
-        d: "None of the above",
-        correct:"c",
-    },
-    {
-        questions: "Which element is used for or styling HTML layout?",
-        a: "CSS" ,
-        b: "jQuery",
-        c: "Javascript",
-        d: "PHP",
-        correct:"a",
-    },
-    {
-        questions: "What does <ul> stand for?",
-        a: "Ordered list" ,
-        b: "Unordered list",
-        c: "Undefined",
-        d: "None of the above",
-        correct:"b",
-    },
-    {
-      questions: "What does CSS stand for?",
-        a: "Cascading Style Sheets." ,
-        b: "Coloured Special Sheets",
-        c: "Color and style sheets",
-        d: "None of the above",
-        correct:"a",  
-    },
-    {
-        questions: "What does && expression mean?",
-        a: "Question" ,
-        b: "Or",
-        c: "Add",
-        d: "None of the above",
-        correct:"d",  
-      },
-]
-
-let questions = document.getElementById("questions")
